1a51df7a4c6fbbd5a2179b1b7c8c494ad07a4021
anu1820 <anurajpatil35@gmail.com>
2023-01-18 19:55:30 +0530

    Method Calling

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using LinkedList_Assingment;
+using System.Collections.Generic;
+
+namespace Linkedlist_Assingment
+{
+    class Program
+    {
+        public  static void Main(string[] args)
+       
+        {
+           
+            LinkedList linkedList = new LinkedList();
+            linkedList.Add(56);
+            linkedList.Add(30);
+            linkedList.Add(70);
+            linkedList.Display();
+        }
+    }
+}
